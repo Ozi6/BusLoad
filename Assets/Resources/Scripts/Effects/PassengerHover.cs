d7f378646fec81bd0e454d1ea47fdc4b93d67bee
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2025-08-05 00:25:40 +0300

    Passenger Hover Test

---
+++ Assets/Resources/Scripts/Effects/PassengerHover.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PassengerHover : MonoBehaviour
+{
+    private Renderer[] renderers;
+    private Material[] originalMaterials;
+    private Material[] outlineMaterials;
+    private bool isHovered = false;
+
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        originalMaterials = new Material[renderers.Length];
+        outlineMaterials = new Material[renderers.Length];
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            originalMaterials[i] = renderers[i].material;
+            outlineMaterials[i] = CreateOutlineMaterial(originalMaterials[i]);
+        }
+    }
+
+    private Material CreateOutlineMaterial(Material original)
+    {
+        Material outline = new Material(original);
+        outline.color = Color.white;
+        outline.SetFloat("_Metallic", 0f);
+        outline.SetFloat("_Smoothness", 1f);
+        outline.EnableKeyword("_EMISSION");
+        outline.SetColor("_EmissionColor", Color.white * 0.5f);
+        return outline;
+    }
+
+    void OnMouseEnter()
+    {
+        SetHovered(true);
+    }
+
+    void OnMouseExit()
+    {
+        SetHovered(false);
+    }
+
+    void SetHovered(bool hovered)
+    {
+        if (isHovered == hovered) return;
+        isHovered = hovered;
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material = hovered ? outlineMaterials[i] : originalMaterials[i];
+        }
+    }
+}
