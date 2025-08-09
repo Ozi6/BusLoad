6d9711635bab75d1d00543d7e1fbcb6ee6aa53b1
Ozi <ozannurcan4@gmail.com>
2025-08-09 13:28:37 +0300

    CloakedTrait Done

---
+++ Assets/Resources/Scripts/ModificationClasses/PassengerTraits/CloakedTrait.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CloakedTrait : PassengerTrait
+{
+    private Passenger owner;
+    [SerializeField] private bool isCloaked = true;
+
+    private void Start()
+    {
+        owner = GetComponent<Passenger>();
+    }
+
+    public override void OnSelected(Passenger passenger)
+    {
+        if (isCloaked)
+            return;
+    }
+
+    public override bool CanBoard(Passenger passenger, Bus bus)
+    {
+        return !isCloaked;
+    }
+
+    protected override void OnNearbyPassengerSelected(Vector2Int clickedPosition)
+    {
+        if (owner == null)
+            return;
+        if(clickedPosition != owner.GridPosition)
+            ToggleCloak();
+    }
+
+    private void ToggleCloak()
+    {
+        isCloaked = !isCloaked;
+    }
+
+    public bool IsCloaked => isCloaked;
+}
